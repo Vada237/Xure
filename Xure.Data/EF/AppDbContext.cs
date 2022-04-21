dfc57712cba6d859383c78c2fea03adc597f25ac
Denis le <denis.le01@mail.ru>
2022-04-21 03:11:53 +0300

    Создал контекст базы данных и создал связи между таблицами

---
+++ Xure.Data/EF/AppDbContext.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Xure.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public DbSet<Category> Categories;
+        public DbSet<Company> Companies;
+        public DbSet<Message> Messages;
+        public DbSet<Order> Orders;
+        public DbSet<OrderProduct> OrderProducts;
+        public DbSet<OrderReport> OrderReports;
+        public DbSet<Product> Products;
+        public DbSet<ProductReport> ProductReports;
+        public DbSet<Reason> Reasons;
+        public DbSet<ReceptionPoint> ReceptionPoints;
+        public DbSet<Reviews> Reviews;
+        
+        
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+                
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Product>(
+            c => {
+                c.HasOne(c => c.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(c => c.CategoryId);
+                c.HasOne(c => c.Seller)
+                .WithMany(c => c.Products)
+                .HasForeignKey(c => c.SellerId);
+            });
+            builder.Entity<Company>(
+               c =>
+                  {
+                      c.HasMany(c => c.Sellers)
+                        .WithOne(c => c.Company)
+                        .HasForeignKey(c => c.CompanyId);
+                  });
+            builder.Entity<Message>(
+                c =>
+                {
+                    c.HasOne(c => c.Client)
+                    .WithMany(c => c.ClientMessages)
+                    .HasForeignKey(c => c.ClientId)
+                    .OnDelete(DeleteBehavior.NoAction);
+                    c.HasOne(c => c.Seller)
+                    .WithMany(c => c.SellerMessages)
+                    .HasForeignKey(c => c.SellerId)
+                    .OnDelete(DeleteBehavior.NoAction);
+                });
+            builder.Entity<Order>(
+                c =>
+                {
+                    c.HasOne(c => c.ReceptionPoint)
+                    .WithMany(c => c.Orders)
+                    .HasForeignKey(c => c.ReceptionPointId);
+                    c.HasOne(c => c.Client)
+                    .WithMany(c => c.Orders)
+                    .HasForeignKey(c => c.ClientId);
+
+                    c.HasMany(c => c.Products)
+                    .WithMany(c => c.Orders)
+                    .UsingEntity<OrderProduct>(
+                        c => c.HasOne(c => c.Product)
+                        .WithMany(c => c.OrderProducts)
+                        .HasForeignKey(c => c.ProductId),
+                        c => c.HasOne(c => c.Order)
+                        .WithMany(c => c.OrderProducts)
+                        .HasForeignKey(c => c.OrderId));
+                });
+            builder.Entity<Reviews>(
+                c => {
+                    c.HasOne(c => c.Client)
+                    .WithMany(c => c.Reviews)
+                    .HasForeignKey(c => c.ClientId);
+                    c.HasOne(c => c.Product)
+                    .WithMany(c => c.Reviews)
+                    .HasForeignKey(c => c.ProductId);
+                });
+            builder.Entity<OrderReport>(
+                c =>
+                {
+                    c.HasOne(c => c.Order)
+                   .WithMany(c => c.OrderReports)
+                   .HasForeignKey(c => c.OrderId);
+                    c.HasOne(c => c.Reason)
+                    .WithMany(c => c.OrderReports)
+                    .HasForeignKey(c => c.ReasonId);
+                });
+            builder.Entity<ProductReport>(
+                c =>
+                {
+                    c.HasOne(c => c.Product)
+                    .WithMany(c => c.ProductReports)
+                    .HasForeignKey(c => c.ProductId);
+                    c.HasOne(c => c.Reason)
+                    .WithMany(c => c.ProductReports)
+                    .HasForeignKey(c => c.ReasonId);
+                });
+        }        
+    }
+}
---
+++ Xure.Data/EF/AppUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Xure.Data
+{
+    public class AppUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Passport { get; set; }
+        public bool Confirmed { get; set; }
+        public string Avatar { get; set; }
+        public Company Company { get; set; }
+        public long CompanyId { get; set; }
+        public List<Order> Orders { get; set; }
+        public List<Message> ClientMessages { get; set; }
+        public List<Message> SellerMessages { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Reviews> Reviews { get; set; }
+    }
+}
