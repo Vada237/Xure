2c2adfd5889f5301cf983a8cc00757f49807e728
Denis le <denis.le01@mail.ru>
2022-05-22 23:29:05 +0300

    Добавлены модели пользователя

---
+++ Xure.Data/Users/Authorization/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Xure.Data
+{
+    public class LoginModel
+    {
+        [Required]  
+        [UIHint("Логин")]
+        public string Email { get; set; }
+
+        [Required]
+        [UIHint("Пароль")]
+        public string Password { get; set; }
+    }
+}
---
+++ Xure.Data/Users/Authorization/СreateModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Xure.Data
+{
+    public class СreateModel
+    {                
+        [UIHint("Номер телефона")]
+        [Required(ErrorMessage = "Укажите номер телефона")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Укажите имя")]
+        [UIHint("Имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Укажите фамилию")]
+        [UIHint("Фамилия")]
+        public string Surname { get; set; }
+
+        [UIHint("Отчество")]        
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Укажите дату рождения")]
+        [UIHint("Дата рождения")]
+        public DateTime Birthday { get; set; }
+
+        [Required(ErrorMessage = "Укажите срию и номер пасспорта")]
+        [UIHint("Серия и номер паспорта")]
+        public string Passport { get; set; }
+        public bool Confirmed { get; set; }
+
+        [UIHint("Аватар")]
+        public string Avatar { get; set; }
+                
+        [UIHint("Почта")]
+        [Required(ErrorMessage = "Укажите почту")]
+        public string Email { get; set; }
+        
+        [UIHint("Пароль")]
+        [Required(ErrorMessage = "Укажите пароль")]
+        public string Password { get; set; }
+    }
+}
