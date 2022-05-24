8070e9c2ed69a4d5f541dd7310f9a524fb82f6ff
Denis le <denis.le01@mail.ru>
2022-05-24 20:16:56 +0300

    Добавлен интерфейс для взаимодействия с пользователями

---
+++ Xure.Api/Interfaces/Users/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Xure.Api.Services
+{
+    public interface IUserRepository
+    {
+        IEnumerable<IdentityUser> GetUsers();        
+    }
+}
