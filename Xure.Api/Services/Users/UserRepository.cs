19d6abcf1401ebb8babd9f79c52416de9b2e1a68
Denis le <denis.le01@mail.ru>
2022-05-24 20:16:19 +0300

    Добавлен сервис для взаимодействия с клиентами

---
+++ Xure.Api/Services/Users/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Xure.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xure.Api.Services
+{
+    public class UserRepository : IUserRepository
+    {
+        private AppDbContext AppDbContext { get; set; }        
+        public UserRepository(AppDbContext context)
+        {
+            AppDbContext = context;
+        }
+
+        public IEnumerable<IdentityUser> GetUsers()
+        {     
+            if (AppDbContext.Users == null)
+            {
+                return null;
+            }
+            return AppDbContext.Users;
+        }
+    }
+}
