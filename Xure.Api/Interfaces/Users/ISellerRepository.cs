e4289e5e643a7cb02f712128f23978459dafa5b7
Denis le <denis.le01@mail.ru>
2022-05-26 01:50:57 +0300

    Добавлен интерфейс для сервиса для продавцов

---
+++ Xure.Api/Interfaces/Users/ISellerRepository.cs
@@ -0,0 +1,9 @@
+﻿using Xure.Data;
+
+namespace Xure.Api.Interfaces {
+    public interface ISellerRepository { 
+        public Company FindCompanyByEmail(string email);
+        public Company FindCompanyByPhone(string phone);
+        public Company FindCompanyByNameAndSurname(string Name,string Surname);
+    }
+}
