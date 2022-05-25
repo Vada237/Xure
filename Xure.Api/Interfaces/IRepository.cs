4754f57d96a450ae0cfb0b3b0fc803d48d1a7575
Denis le <denis.le01@mail.ru>
2022-05-26 01:50:09 +0300

    Обновлен общий репозиторий с добавлением поиска с подключением связанных таблиц

---
+++ Xure.Api/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xure.Api.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();                     
+        void Update(T item);
+        void Create(T item);
+        void Delete(int id);
+        void Save();               
+    }    
+}
