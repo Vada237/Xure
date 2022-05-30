97686c0466e5befe081363d4df7d74685489bcd2
Denis le <denis.le01@mail.ru>
2022-05-31 00:35:12 +0300

    Добавлены ViewModels для регистрации клиентов и поставщиков

---
+++ Xure/Models/CompanyViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+
+namespace Xure.App.Models
+{
+    public class CompanyViewModel
+    {        
+        [Required(ErrorMessage = "Введите название магазина")]
+        [MaxLength(100, ErrorMessage = "Название компании не должна превышать 100 символов")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Введите описание магазина")]
+        [MinLength(30, ErrorMessage = "Описание магазина не должно быть меньше 30 символов")]
+        [MaxLength(200, ErrorMessage = "Описание магазина не должно превышать 200 символов")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Введите дату регистрации магазина")]
+        public DateTime DateRegistration { get; set; }
+
+        [Required(ErrorMessage = "Введите ИНН")]
+        [MaxLength(20, ErrorMessage = "Длина ИНН не должна превышать 20 символов")]
+        public string INN { get; set; }
+
+        [Required(ErrorMessage = "Введите ОГРН")]
+        [MaxLength(50, ErrorMessage = "Длина ОГРН не должна превышать 50 символов")]
+        public string OGRN { get; set; }        
+    }
+}
---
+++ Xure/Models/UserViewModel.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Xure.App.Models
+{
+    public class UserViewModel
+    {
+        [UIHint("Номер телефона")]
+        [Required(ErrorMessage = "Укажите номер телефона")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Укажите имя")]
+        [UIHint("Имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Укажите фамилию")]
+        [UIHint("Фамилия")]
+        public string Surname { get; set; }
+
+        [UIHint("Отчество")]
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Укажите дату рождения")]
+        [UIHint("Дата рождения")]
+        public DateTime Birthday { get; set; }
+
+        [Required(ErrorMessage = "Укажите серию и номер пасспорта")]
+        [UIHint("Серия и номер паспорта")]
+        public string Passport { get; set; }
+        public bool Confirmed { get; set; }
+
+        [Required(ErrorMessage = "Добавьте аватар")]
+        [FileExtensions(Extensions = "jpg,jpeg,png", ErrorMessage = "Поддерживаются только форматы: jpg, png, jpeg")]
+        [UIHint("Аватар")]
+        public IFormFile Avatar { get; set; }
+
+        [UIHint("Почта")]
+        [Required(ErrorMessage = "Укажите почту")]
+        public string Email { get; set; }
+
+        [UIHint("Пароль")]
+        [Required(ErrorMessage = "Укажите пароль")]
+        public string Password { get; set; }
+    }
+
+    class CreateClientViewModel
+    {
+        [UIHint("Номер телефона")]
+        [Required(ErrorMessage = "Укажите номер телефона")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Укажите имя")]
+        [UIHint("Имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Укажите фамилию")]
+        [UIHint("Фамилия")]
+        public string Surname { get; set; }
+
+        [UIHint("Отчество")]
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Укажите дату рождения")]
+        [UIHint("Дата рождения")]
+        public DateTime Birthday { get; set; }
+
+        [Required(ErrorMessage = "Укажите серию и номер пасспорта")]
+        [UIHint("Серия и номер паспорта")]
+        public string Passport { get; set; }
+        public bool Confirmed { get; set; }               
+
+        [UIHint("Почта")]
+        [Required(ErrorMessage = "Укажите почту")]
+        public string Email { get; set; }
+
+        [UIHint("Пароль")]
+        [Required(ErrorMessage = "Укажите пароль")]
+        public string Password { get; set; }
+    }
+
+    public class CreateSellerViewModel
+    {
+        public UserViewModel User { get; set; }
+        public CompanyViewModel Company { get; set; }
+    }
+}
