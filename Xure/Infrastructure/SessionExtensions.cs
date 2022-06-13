32097afea2ccdd0918f54249ff4cd48827abe9c2
Denis le <denis.le01@mail.ru>
2022-06-13 14:43:01 +0300

    Добавлен сервис для покупательской корзины

---
+++ Xure/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Xure.App.Infrastructure
+{
+    public static class SessionExtensions           
+        {
+            public static void SetJson(this ISession session, string key, object value)
+            {
+                var settings = new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                };
+
+                session.SetString(key, JsonConvert.SerializeObject(value));
+            }
+            public static T GetJson<T>(this ISession session, string key)
+            {
+                var value = session.GetString(key);
+                return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+            }
+        }
+}
