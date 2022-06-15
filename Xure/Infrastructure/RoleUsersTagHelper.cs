05e7dadbe06a85ee182a4f8327fa5aada6fc3f53
Denis le <denis.le01@mail.ru>
2022-06-15 16:14:18 +0300

    Добавлен компонент для отображений пользователей данной роли

---
+++ Xure/Infrastructure/RoleUsersTagHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Xure.Data;
+using Xure.App.Models;
+
+namespace Xure.App.Infrastructure
+{
+
+    [HtmlTargetElement("td", Attributes = "identity-role")]
+    public class RoleUsersTagHelper : TagHelper
+    {
+        private UserManager<AppUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public RoleUsersTagHelper(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+        [HtmlAttributeName("identity-role")]
+        public string Role { get; set; }
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> names = new List<string>();
+            IdentityRole role = await roleManager.FindByIdAsync(Role);
+            if (role != null)
+            {
+                foreach (var user in userManager.Users)
+                {
+                    if (user != null && await userManager.IsInRoleAsync(user, role.Name))
+                    {
+                        names.Add(user.UserName);
+                    }
+                }
+            }
+            output.Content.SetContent(names.Count == 0 ? "Нет пользователей" : string.Join(", ", names));
+        }
+    }
+}
