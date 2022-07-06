cd81b3c8e5a32d8e965af9d5fe0c6dd460aad7fe
Denis le <denis.le01@mail.ru>
2022-07-06 21:38:06 +0300

    Добавлены модели представлений для сброса пароля

---
+++ Xure/Models/Users/PasswordViewModels.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Xure.App.Models
+{
+    public class ForgotPasswordViewModel { 
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+
+    public class ResetPasswordViewModel { 
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+}
