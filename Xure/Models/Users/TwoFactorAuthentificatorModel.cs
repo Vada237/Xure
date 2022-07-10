2ddf081c80e661ec047e649da3ed996e8285e2a0
Denis le <denis.le01@mail.ru>
2022-07-10 10:26:05 +0300

    Добавлена модель для двухэтапной аутентификации

---
+++ Xure/Models/Users/TwoFactorAuthentificatorModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Xure.App.Models
+{
+    public class TwoFactorAuthentificatorModel
+    {
+        public string AuthenticatorKey { get; set; }
+        public string Code { get; set; }
+    }
+
+    public class LoginTwoStepModel { 
+        public string ReturnUrl { get; set; }
+        public string Token { get; set; }        
+    }
+}
